8c3db32debcc9eb414c42f1e87e08f0c0d4a6882
Willem Jacobs <wamj58@gmail.com>
2021-05-11 23:20:09 -0500

    Initial push to branch

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 console.log('Hello World');
 
-let hours = ['6am', '7am', '8am'];
+let hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '];
 
 let seattle = {
   name: 'Seattle',
