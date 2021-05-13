228f0a628d4a4749c3de871be4f12a450a4b8802
Willem Jacobs <wamj58@gmail.com>
2021-05-12 21:55:01 -0500

    Final on lab-06 Object Literals

--- js/app.js
+++ js/app.js
@@ -2,9 +2,12 @@
 
 // setup hours array of stores operational hours
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-let totalCookiesCompany = [];
 
-// grab the <ul> node from the DOM to append to later
+// Not in scope - added a company total array for later usage.
+let totalCookiesCompany = [];
+let companyTotalByHour = [0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+// grab the <div> node from the DOM to append to later
 let listContainer = document.getElementById('data-container');
 
 // Seattle Store Object
@@ -19,35 +22,40 @@ let seattle = {
   randomCustomersPerHour: function () {
     return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour);
   },
-  calcCookiesPerDay: function() {
+  calcCookierPerHour: function() {
     // Build the data needed based on properties of object
     let cookiesByHour = 0;
     for (let i = 0; i < hours.length; i++) {
       cookiesByHour = Math.round(this.randomCustomersPerHour() * this.avgCookiesPerCustomer);
       this.cookiesByHourArray.push(cookiesByHour);
       this.dailyCookieTotal += cookiesByHour;
+      // line below for grand total - out of scope
+      companyTotalByHour[i] = companyTotalByHour[i] + cookiesByHour;
     }
   },
   render: function() {
-    // run calcCookiesPerDay function to build data line to display
-    this.calcCookiesPerDay();
+    // run calcCookierPerHour function to build data line to display
+    this.calcCookierPerHour();
     let dataArray = [];
     for (let i = 0; i < hours.length; i++) {
       let dataLine = `${hours[i]}: ${this.cookiesByHourArray[i]} cookies`;
       dataArray.push(dataLine);
     }
     dataArray.push(`Total: ${this.dailyCookieTotal} cookies`);
+    // 2 lines below for grand total - out of scope.
+    totalCookiesCompany.push(this.storeName);
     totalCookiesCompany.push(this.dailyCookieTotal);
 
-    // build DOM elements required
+    // build DOM elements required using a section for each object
     let storeSection = document.createElement('section');
+    // setting a class for future styling
     storeSection.className = 'data-section';
     let storeNameH2El = document.createElement('h2');
     let ulEl = document.createElement('ul');
     storeNameH2El.textContent = this.storeName;
+    listContainer.appendChild(storeSection);
     storeSection.appendChild(storeNameH2El);
     storeSection.appendChild(ulEl);
-    listContainer.appendChild(storeSection);
 
     // loop to add data to the list
     for (let i = 0; i < dataArray.length; i++) {
@@ -70,35 +78,40 @@ let tokyo = {
   randomCustomersPerHour: function () {
     return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour);
   },
-  calcCookiesPerDay: function() {
+  calcCookierPerHour: function() {
     // Build the data needed based on properties of object
     let cookiesByHour = 0;
     for (let i = 0; i < hours.length; i++) {
       cookiesByHour = Math.round(this.randomCustomersPerHour() * this.avgCookiesPerCustomer);
       this.cookiesByHourArray.push(cookiesByHour);
       this.dailyCookieTotal += cookiesByHour;
+      // line below for grand total - out of scope
+      companyTotalByHour[i] = companyTotalByHour[i] + cookiesByHour;
     }
   },
   render: function() {
-    // run calCookiesPerDay function to build data
-    this.calcCookiesPerDay();
+    // run calcCookierPerHour function to build data line to display
+    this.calcCookierPerHour();
     let dataArray = [];
     for (let i = 0; i < hours.length; i++) {
       let dataLine = `${hours[i]}: ${this.cookiesByHourArray[i]} cookies`;
       dataArray.push(dataLine);
     }
     dataArray.push(`Total: ${this.dailyCookieTotal} cookies`);
+    // 2 lines below for grand total - out of scope.
+    totalCookiesCompany.push(this.storeName);
     totalCookiesCompany.push(this.dailyCookieTotal);
 
-    // build DOM elements required
+    // build DOM elements required using a section for each object
     let storeSection = document.createElement('section');
+    // setting a class for future styling
     storeSection.className = 'data-section';
     let storeNameH2El = document.createElement('h2');
     let ulEl = document.createElement('ul');
     storeNameH2El.textContent = this.storeName;
+    listContainer.appendChild(storeSection);
     storeSection.appendChild(storeNameH2El);
     storeSection.appendChild(ulEl);
-    listContainer.appendChild(storeSection);
 
     // loop to add data to the list
     for (let i = 0; i < dataArray.length; i++) {
@@ -121,35 +134,40 @@ let dubai = {
   randomCustomersPerHour: function () {
     return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour);
   },
-  calcCookiesPerDay: function() {
+  calcCookierPerHour: function() {
     // Build the data needed based on properties of object
     let cookiesByHour = 0;
     for (let i = 0; i < hours.length; i++) {
       cookiesByHour = Math.round(this.randomCustomersPerHour() * this.avgCookiesPerCustomer);
       this.cookiesByHourArray.push(cookiesByHour);
       this.dailyCookieTotal += cookiesByHour;
+      // line below for grand total - out of scope
+      companyTotalByHour[i] = companyTotalByHour[i] + cookiesByHour;
     }
   },
   render: function() {
-    // run calCookiesPerDay function to build data
-    this.calcCookiesPerDay();
+    // run calcCookierPerHour function to build data line to display
+    this.calcCookierPerHour();
     let dataArray = [];
     for (let i = 0; i < hours.length; i++) {
       let dataLine = `${hours[i]}: ${this.cookiesByHourArray[i]} cookies`;
       dataArray.push(dataLine);
     }
     dataArray.push(`Total: ${this.dailyCookieTotal} cookies`);
+    // 2 lines below for grand total - out of scope.
+    totalCookiesCompany.push(this.storeName);
     totalCookiesCompany.push(this.dailyCookieTotal);
 
-    // build DOM elements required
+    // build DOM elements required using a section for each object
     let storeSection = document.createElement('section');
+    // setting a class for future styling
     storeSection.className = 'data-section';
     let storeNameH2El = document.createElement('h2');
     let ulEl = document.createElement('ul');
     storeNameH2El.textContent = this.storeName;
+    listContainer.appendChild(storeSection);
     storeSection.appendChild(storeNameH2El);
     storeSection.appendChild(ulEl);
-    listContainer.appendChild(storeSection);
 
     // loop to add data to the list
     for (let i = 0; i < dataArray.length; i++) {
@@ -172,35 +190,40 @@ let paris = {
   randomCustomersPerHour: function () {
     return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour);
   },
-  calcCookiesPerDay: function() {
+  calcCookierPerHour: function() {
     // Build the data needed based on properties of object
     let cookiesByHour = 0;
     for (let i = 0; i < hours.length; i++) {
       cookiesByHour = Math.round(this.randomCustomersPerHour() * this.avgCookiesPerCustomer);
       this.cookiesByHourArray.push(cookiesByHour);
       this.dailyCookieTotal += cookiesByHour;
+      // line below for grand total - out of scope
+      companyTotalByHour[i] = companyTotalByHour[i] + cookiesByHour;
     }
   },
   render: function() {
-    // run calCookiesPerDay function to build data
-    this.calcCookiesPerDay();
+    // run calcCookierPerHour function to build data line to display
+    this.calcCookierPerHour();
     let dataArray = [];
     for (let i = 0; i < hours.length; i++) {
       let dataLine = `${hours[i]}: ${this.cookiesByHourArray[i]} cookies`;
       dataArray.push(dataLine);
     }
     dataArray.push(`Total: ${this.dailyCookieTotal} cookies`);
+    // 2 lines below for grand total - out of scope.
+    totalCookiesCompany.push(this.storeName);
     totalCookiesCompany.push(this.dailyCookieTotal);
 
-    // build DOM elements required
+    // build DOM elements required using a section for each object
     let storeSection = document.createElement('section');
+    // setting a class for future styling
     storeSection.className = 'data-section';
     let storeNameH2El = document.createElement('h2');
     let ulEl = document.createElement('ul');
     storeNameH2El.textContent = this.storeName;
+    listContainer.appendChild(storeSection);
     storeSection.appendChild(storeNameH2El);
     storeSection.appendChild(ulEl);
-    listContainer.appendChild(storeSection);
 
     // loop to add data to the list
     for (let i = 0; i < dataArray.length; i++) {
@@ -223,35 +246,40 @@ let lima = {
   randomCustomersPerHour: function () {
     return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour);
   },
-  calcCookiesPerDay: function() {
+  calcCookierPerHour: function() {
     // Build the data needed based on properties of object
     let cookiesByHour = 0;
     for (let i = 0; i < hours.length; i++) {
       cookiesByHour = Math.round(this.randomCustomersPerHour() * this.avgCookiesPerCustomer);
       this.cookiesByHourArray.push(cookiesByHour);
       this.dailyCookieTotal += cookiesByHour;
+      // line below for grand total - out of scope
+      companyTotalByHour[i] = companyTotalByHour[i] + cookiesByHour;
     }
   },
   render: function() {
-    // run calCookiesPerDay function to build data
-    this.calcCookiesPerDay();
+    // run calcCookierPerHour function to build data line to display
+    this.calcCookierPerHour();
     let dataArray = [];
     for (let i = 0; i < hours.length; i++) {
       let dataLine = `${hours[i]}: ${this.cookiesByHourArray[i]} cookies`;
       dataArray.push(dataLine);
     }
     dataArray.push(`Total: ${this.dailyCookieTotal} cookies`);
+    // 2 lines below for grand total - out of scope.
+    totalCookiesCompany.push(this.storeName);
     totalCookiesCompany.push(this.dailyCookieTotal);
 
-    // build DOM elements required
+    // build DOM elements required using a section for each object
     let storeSection = document.createElement('section');
+    // setting a class for future styling
     storeSection.className = 'data-section';
     let storeNameH2El = document.createElement('h2');
     let ulEl = document.createElement('ul');
     storeNameH2El.textContent = this.storeName;
+    listContainer.appendChild(storeSection);
     storeSection.appendChild(storeNameH2El);
     storeSection.appendChild(ulEl);
-    listContainer.appendChild(storeSection);
 
     // loop to add data to the list
     for (let i = 0; i < dataArray.length; i++) {
@@ -267,4 +295,7 @@ tokyo.render();
 dubai.render();
 paris.render();
 lima.render();
+
+// This below is some testing I am doing with grand totals. Not part of project at this point.
 console.log(totalCookiesCompany);
+console.log(companyTotalByHour);
